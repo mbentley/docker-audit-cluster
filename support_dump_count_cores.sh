62bd79a7dff8b413966d670f392f11c6fbde2d5c
Matt Bentley <mbentley@mbentley.net>
2020-05-15 15:34:24 -0400

    Updates to scripts
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

---
+++ support_dump_count_cores.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+nanoCPUs="$(grep NanoCPUs ucp-nodes.txt | awk '{print $2}' | awk -F ',' '{print $1}')" &&\
+CPUs=$(for i in ${nanoCPUs}; do   echo "$((i/1000000000))"; done) &&\
+ttlCPU="$(count=0; total=0; for i in ${CPUs};do total=$(echo "$total+$i" | bc ); ((count++)); done; echo "${total}")"
+node_count=$(echo "${CPUs}" | wc -l | tr -d " ")
+avgCPU="$(echo "scale=2; $ttlCPU / $node_count" | bc)"
+minCPU="$(echo "${CPUs}" | sort -n | head -1)"
+maxCPU="$(echo "${CPUs}" | sort -n | tail -n 1)"
+CPU_sizes="$(echo "${CPUs}" | sort -n | uniq)"
+
+# CPU size report
+for size in ${CPU_sizes}
+do
+  echo -n "${size} Core x "
+  echo "${CPUs}" | grep "${size}" | sort -n | wc -l | tr -d " "
+done
+
+echo
+
+echo "# Nodes - ${node_count}"
+echo "Ttl Core - ${ttlCPU}"
+echo "Min Core - ${minCPU}"
+echo "Max Core - ${maxCPU}"
+echo "Avg Core - ${avgCPU}"
--- swarm_core_audit.sh
+++ swarm_core_audit.sh
@@ -20,38 +20,57 @@ then
   exit 1
 fi
 
-# shellcheck disable=SC2086
-# get the number of nano CPUs reported from Swarm on each node
-nanoCPUs="$(for NODE in $(${CURL_CMD}/nodes | jq -r '.[]|.ID'); do ${CURL_CMD}/nodes/"${NODE}" | jq -r .Description.Resources.NanoCPUs; done)"
+pull_node_data() {
+  # output for type
+  echo -e "Data for ${1} nodes:"
 
-# convery nano CPUs to CPUs
-CPUs=$(for i in ${nanoCPUs}; do   echo "$((i/1000000000))"; done)
+  # add filter for managers or workers
+  if [ "${1}" = "manager" ] || [ "${1}" = "worker" ]
+  then
+    NODE_FILTER="?filters=%7B%22role%22%3A%7B%22${1}%22%3Atrue%7D%7D"
+  fi
 
-# get the sum of all CPU counts
-ttlCPU="$(COUNT=0; TOTAL=0; for i in ${CPUs};do TOTAL=$(echo "${TOTAL}+${i}" | bc ); ((COUNT++)); done; echo "${TOTAL}")"
+  # shellcheck disable=SC2086
+  # get the number of nano CPUs reported from Swarm on each node
+  nanoCPUs="$(for NODE in $(${CURL_CMD}/nodes${NODE_FILTER} | jq -r '.[]|.ID'); do ${CURL_CMD}/nodes/"${NODE}" | jq -r .Description.Resources.NanoCPUs; done)"
 
-# count the number of nodes reported
-NODE_COUNT=$(echo "${CPUs}" | wc -l)
+  # convery nano CPUs to CPUs
+  CPUs=$(for i in ${nanoCPUs}; do   echo "$((i/1000000000))"; done)
 
-# find the average CPU count
-avgCPU="$(echo "scale=2; ${ttlCPU} / ${NODE_COUNT}" | bc)"
+  # get the sum of all CPU counts
+  ttlCPU="$(COUNT=0; TOTAL=0; for i in ${CPUs};do TOTAL=$(echo "${TOTAL}+${i}" | bc ); ((COUNT++)); done; echo "${TOTAL}")"
 
-# determine the smallest # CPUs per node
-minCPU="$(echo "${CPUs}" | sort -n | head -1)"
+  # count the number of nodes reported
+  NODE_COUNT=$(echo "${CPUs}" | wc -l)
 
-# determine the largest # CPUs per node
-maxCPU="$(echo "${CPUs}" | sort -n | tail -n 1)"
+  # find the average CPU count
+  avgCPU="$(echo "scale=2; ${ttlCPU} / ${NODE_COUNT}" | bc)"
 
-# find the unique CPU node sizes
-CPU_sizes="$(echo "${CPUs}" | sort -n | uniq)"
+  # determine the smallest # CPUs per node
+  minCPU="$(echo "${CPUs}" | sort -n | head -1)"
 
-# report the quantity of nodes that match a given number of CPUs
-for SIZE in ${CPU_sizes}; do echo -n "${SIZE} Core x "; echo "${CPUs}" | grep "${SIZE}" | sort -n | wc -l; done
+  # determine the largest # CPUs per node
+  maxCPU="$(echo "${CPUs}" | sort -n | tail -n 1)"
 
-# report the rest of the data
-echo "
+  # find the unique CPU node sizes
+  CPU_sizes="$(echo "${CPUs}" | sort -n | uniq)"
+
+  # report the quantity of nodes that match a given number of CPUs
+  for SIZE in ${CPU_sizes}; do echo -n "${SIZE} Core x "; echo "${CPUs}" | grep "${SIZE}" | sort -n | wc -l; done
+
+  # report the rest of the data
+  echo "
 # Nodes - ${NODE_COUNT}
 Ttl Core - ${ttlCPU}
 Min Core - ${minCPU}
 Max Core - ${maxCPU}
 Avg Core - ${avgCPU}"
+}
+
+echo "========================"
+pull_node_data all
+echo "========================"
+pull_node_data manager
+echo "========================"
+pull_node_data worker
+echo "========================"
